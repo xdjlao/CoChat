79f4f465d5d21fddf9d3ca4c1be0d88081786200
xdjlao <jerry@flatie.com>
2016-02-23 11:05:15 -0800

    create and refactor whisper storyboard

---
+++ CoChat/WhisperTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WhisperTableViewCell.swift
+//  CoChat
+//
+//  Created by Jerry on 2/23/16.
+//  Copyright © 2016 Jerry. All rights reserved.
+//
+
+import UIKit
+
+class WhisperTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
