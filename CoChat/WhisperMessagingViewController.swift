1ffb70bd7922222dfd0de6dac53b47ee84ef2efa
xdjlao <jerry@flatie.com>
2016-02-23 12:51:38 -0800

    update

--- CoChat/WhisperMessagingViewController.swift
+++ CoChat/WhisperMessagingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  WhisperViewController.swift
+//  WhisperMessagingViewController.swift
 //  CoChat
 //
 //  Created by Jerry on 2/23/16.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class WhisperViewController: UIViewController, UITableViewDataSource {
+class WhisperMessagingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,15 +21,9 @@ class WhisperViewController: UIViewController, UITableViewDataSource {
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+    @IBAction func onCloseButtonTapped(sender: UIBarButtonItem) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("WhisperCell", forIndexPath: indexPath)
-        return cell
-    }
-    
 
     /*
     // MARK: - Navigation
