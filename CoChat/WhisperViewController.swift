092b6b1966416c379ca27dce5e42aecaff7347d1
Aaron Bikis <aaron.bikis@gmail.com>
2016-02-28 21:56:37 -0800

    added whisperVC UI

--- CoChat/WhisperViewController.swift
+++ CoChat/WhisperViewController.swift
@@ -1,16 +1,26 @@
 import UIKit
 
-class WhisperViewController: UIViewController, UITableViewDataSource {
-
-   
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
+class WhisperViewController: UIViewController {
+    @IBOutlet var tableView: UITableView!
+ var whispers = [Whisper]()
     
+}
+
+
+extension WhisperViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("WhisperCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("WhisperCell", forIndexPath: indexPath) as? WhisperCell
+        cell?.nameLabel.text = whispers[indexPath.row].sender.name
+        cell?.messageLabel.text = whispers[indexPath.row].text
+        cell?.profileImageView.image = UIImage(named: "dummyImage")
         return cell
     }
-    
 
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return whispers.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    }
 }
