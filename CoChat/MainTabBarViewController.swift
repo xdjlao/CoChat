902f7f7aa8697988f009b1de2deba4b93161095c
Aaron Bikis <aaron.bikis@gmail.com>
2016-03-01 17:18:25 -0800

    tabBar Iconography

--- CoChat/MainTabBarViewController.swift
+++ CoChat/MainTabBarViewController.swift
@@ -9,31 +9,45 @@
 import UIKit
 
 class MainTabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTabBarUI()
+    }
+    
+    func setupTabBarUI(){
+        let tabBar = self.tabBar
+        guard let tabBarItem1 = tabBar.items?[0] else {return print("couldn't locate TB1image")}
+        guard let tabBarItem2 = tabBar.items?[1] else {return print("couldn't locate TB2image")}
+        guard let tabBarItem3 = tabBar.items?[2] else {return print("couldn't locate TB3image")}
+        guard let tabBarItem4 = tabBar.items?[3] else {return print("couldn't locate TB4image")}
+        
+        tabBarItem1.title = "Blend"
+        tabBarItem2.title = "Whisper"
+        tabBarItem3.title = "Join"
+        tabBarItem4.title = "Profile"
+        
+        tabBarItem1.image = UIImage(named: "blendTabBar")
+        tabBarItem2.image = UIImage(named: "whisper")
+        tabBarItem3.image = UIImage(named: "join")
+        tabBarItem4.image = UIImage(named: "profile")
         tabBar.barTintColor = Theme.Colors.NavigationBarColor.color
         tabBar.translucent = false
         tabBar.setValue(true, forKey: "_hidesShadow")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
         //let sourceViewController = segue.sourceViewController
     }
     
-
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-//        if segue.identifier == "TabToScan" {
-//            //let destination = segue.destinationViewController as! ScanViewController
-//        }
-//    }
-
+    //    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    //        if segue.identifier == "TabToScan" {
+    //            //let destination = segue.destinationViewController as! ScanViewController
+    //        }
+    //    }
+    
 }
